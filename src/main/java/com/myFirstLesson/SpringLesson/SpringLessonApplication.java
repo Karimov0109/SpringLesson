3ceebc9ce16b49093d4ccd0ef51e82fc5aab004f
Karimov_Sunnatillo <shaxa.shaxa.bbbbh@gmail.com>
2022-03-19 19:15:11 +0500

    FirstLesson

---
+++ src/main/java/com/myFirstLesson/SpringLesson/SpringLessonApplication.java
@@ -0,0 +1,11 @@
+package com.myFirstLesson.SpringLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLessonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLessonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myFirstLesson/SpringLesson/SpringLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myFirstLesson.SpringLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
